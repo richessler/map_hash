6497d961729de3bbbe186dc93782aac5db0c2c5a
BYWallace <bwallace90@gmail.com>
2014-07-02 18:39:48 -0400

    moved css and map script into assets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,13 @@
-# class TweetsController < ApplicationController
+class TweetsController < ApplicationController
 
-#   def index
-#     @client = Twitter::REST::Client.new do |config|
-#       config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-#       config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-#       config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-#       config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-#     end
+  def index
+    # @client = Twitter::REST::Client.new do |config|
+    #   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    #   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    #   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    #   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+    # end
 
-#     @tweets = @client.search("#learnedatga", :result_type => "recent").take(5_000)
-#   end
-# end
+    # @tweets = @client.search("#learnedatga", :result_type => "recent").take(5_000)
+  end
+end

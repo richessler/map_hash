2076ccae3ab19f935db9a2dea6e0f0ebd03f7acc
rmlatz <rmlatz@gmail.com>
2014-07-03 16:45:51 -0400

    search and style

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,35 +1,28 @@
 class TweetsController < ApplicationController
 
   def index
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-      config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-    end
+    if params[:q]
+      @client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+        config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+        config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+        config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+      end
 
-    # max_count = 4
-    # count = 0
-    # while count < max_count
-    #   @tweets = @client.search("4th of july", results: 'mixed').take(1_000_000)
-    #   @tweets.each do |tweet|
-    #     if (tweet.attrs[:geo])
-    #       Tweet.create( lat:      tweet.attrs[:geo][:coordinates][0],
-    #                     long:     tweet.attrs[:geo][:coordinates][1],
-    #                     handle:   tweet.attrs[:user][:screen_name],
-    #                     link_id:  tweet.attrs[:id],
-    #                     content:  tweet.text
-    #                     )
-    #     end
-    # end
-    # count += 1
-    # end
+      @search_results_array = []
+      max_count = 3
+      count = 0
 
-    @tweet_array = Tweet.all
+      while count < max_count
+        search_tweets = @client.search("#{params[:q]}", results: 'mixed').take(1000)
+        search_tweets.each do |tweet|
 
-    respond_to do |format|
-      format.html
-      format.json  { render :json => @tweet_array }
-    end
-  end
-end
+          if tweet.attrs[:geo]
+            @search_results_array << tweet
+          end
+
+        end # End do
+      end # End while loop
+    end # End if
+  end # End Def Index
+end # End class

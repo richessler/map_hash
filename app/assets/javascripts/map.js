7a494aa089a929d0e2635d602e8dec9134fb526e
BYWallace <bwallace90@gmail.com>
2014-07-03 15:09:48 -0400

    us map with zoomy states

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,34 +1,78 @@
 $(document).ready(function() {
-  var width = 960,
-    height = 500;
+var width = 960,
+    height = 500,
+    centered;
 
 var projection = d3.geo.albersUsa()
-    .scale(1000)
+    .scale(1070)
     .translate([width / 2, height / 2]);
 
 var path = d3.geo.path()
     .projection(projection);
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select(".container").append("svg")
     .attr("width", width)
     .attr("height", height);
 
+svg.append("rect")
+    .attr("class", "background")
+    .attr("width", width)
+    .attr("height", height)
+    .on("click", clicked);
+
+var g = svg.append("g");
+
 d3.json("/us-10m.json", function(error, us) {
-  svg.insert("path", ".graticule")
-      .datum(topojson.feature(us, us.objects.land))
-      .attr("class", "land")
-      .attr("d", path);
+  g.append("g")
+      .attr("id", "states")
+    .selectAll("path")
+      .data(topojson.feature(us, us.objects.states).features)
+    .enter().append("path")
+      .attr("d", path)
+      .on("click", clicked);
 
-  svg.insert("path", ".graticule")
-      .datum(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b && !(a.id / 1000 ^ b.id / 1000); }))
-      .attr("class", "county-boundary")
-      .attr("d", path);
-
-  svg.insert("path", ".graticule")
+  g.append("path")
       .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-      .attr("class", "state-boundary")
+      .attr("id", "state-borders")
       .attr("d", path);
+
+      d3.csv("/cities.csv", function(error, data) {
+  g.insert("circle", ".graticule")
+     .data(data)
+     .attr("cx", function(d) {
+             return projection([d.lon, d.lat])[0];
+     })
+     .attr("cy", function(d) {
+             return projection([d.lon, d.lat])[1];
+     })
+     .attr("r", 4)
+     .style("fill", "4099FF");
+ });
 });
 
-d3.select(self.frameElement).style("height", height + "px");
+function clicked(d) {
+  var x, y, k;
+
+  if (d && centered !== d) {
+    var centroid = path.centroid(d);
+    x = centroid[0];
+    y = centroid[1];
+    k = 4;
+    centered = d;
+  } else {
+    x = width / 2;
+    y = height / 2;
+    k = 1;
+    centered = null;
+  }
+
+  g.selectAll("path")
+      .classed("active", centered && function(d) { return d === centered; });
+
+  g.transition()
+      .duration(750)
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+      .style("stroke-width", 1.5 / k + "px");
+}
+
 });
